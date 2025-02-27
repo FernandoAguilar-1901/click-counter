91b79c412efd794a2b6343423ca621bf37154571
Fernando Aguilar <ferlaguilar1901@gmail.com>
2025-02-27 11:38:32 -0600

    Improved the user experience by adding messages when you hit certain amount of clicks

--- counter.js
+++ counter.js
@@ -1,10 +1,34 @@
 let setCounter = 0;
+let funnyMessage;
+
 function count() {
     setCounter++;
     document.getElementById('text-count').innerHTML = setCounter;
+    if (setCounter >= 5) {
+        funnyMessage = String("You're good at this!!");
+        document.getElementById('funny-message').innerHTML = funnyMessage;
+    } if (setCounter >= 10) {
+        funnyMessage = String("Keep going!!");
+        document.getElementById('funny-message').innerHTML = funnyMessage;
+    } if (setCounter >= 15) {
+        funnyMessage = String("Impressive!!");
+        document.getElementById('funny-message').innerHTML = funnyMessage;
+    } if (setCounter >= 20){
+        funnyMessage = String("You,re gonna break your mouse...");
+        document.getElementById('funny-message').innerHTML = funnyMessage;
+    } if (setCounter >= 25){
+        funnyMessage = String("Seriously, stop...");
+        document.getElementById('funny-message').innerHTML = funnyMessage;
+    } if (setCounter >= 30){
+        funnyMessage = String("...");
+        document.getElementById('funny-message').innerHTML = funnyMessage;
+    }
 }
 
-function restart() {
+function reset() {
     setCounter = 0;
+    funnyMessage = "";
     document.getElementById('text-count').innerHTML = setCounter;
+    document.getElementById('funny-message').innerHTML = funnyMessage;
+
 }
