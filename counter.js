e62191fdb04842a63f6932083c0d31c70a4dbe7b
FerAguilar <ferlaguilar1901@gmail.com>
2025-04-08 18:19:15 -0600

    Se mejora la legibilidad del codigo

--- counter.js
+++ counter.js
@@ -1,34 +1,46 @@
-let setCounter = 0;
-let funnyMessage;
+let setCounter = 5;
+let textCount = document.getElementById("text-count");
+let funnyMessage = document.getElementById("funny-message");
+let countButton = document.getElementById("count-button");
+let clearButton = document.getElementById("clear-button");
 
-function count() {
+// Funcion para boton de conteo
+countButton.addEventListener("click", e => {
     setCounter++;
-    document.getElementById('text-count').innerHTML = setCounter;
-    if (setCounter >= 5) {
-        funnyMessage = String("You're good at this!!");
-        document.getElementById('funny-message').innerHTML = funnyMessage;
-    } if (setCounter >= 10) {
-        funnyMessage = String("Keep going!!");
-        document.getElementById('funny-message').innerHTML = funnyMessage;
-    } if (setCounter >= 15) {
-        funnyMessage = String("Impressive!!");
-        document.getElementById('funny-message').innerHTML = funnyMessage;
-    } if (setCounter >= 20){
-        funnyMessage = String("You,re gonna break your mouse...");
-        document.getElementById('funny-message').innerHTML = funnyMessage;
-    } if (setCounter >= 25){
-        funnyMessage = String("Seriously, stop...");
-        document.getElementById('funny-message').innerHTML = funnyMessage;
-    } if (setCounter >= 30){
-        funnyMessage = String("...");
-        document.getElementById('funny-message').innerHTML = funnyMessage;
+    textCount.innerText = setCounter;
+
+    switch (setCounter) {
+        case 5:
+            funnyMessage.innerText = "You're good at this!!";
+            break;
+
+        case 10:
+            funnyMessage.innerText = "Keep going!!";
+            break;
+
+        case 15:
+            funnyMessage.innerText = "Impressive!!";
+            break;
+
+        case 20:
+            funnyMessage.innerText = "You,re gonna break your mouse...";
+            break;
+
+        case 25:
+            funnyMessage.innerText = "Seriously, stop...";
+            break;
+
+        case 30:
+            funnyMessage.innerText = "...";
+            break;
+
     }
-}
 
-function reset() {
+});
+
+// Funcion para boton de reseteo
+clearButton.addEventListener("click", e => {
     setCounter = 0;
-    funnyMessage = "";
-    document.getElementById('text-count').innerHTML = setCounter;
-    document.getElementById('funny-message').innerHTML = funnyMessage;
-
-}
+    textCount.innerText = setCounter;
+    funnyMessage.innerText = "";
+});
